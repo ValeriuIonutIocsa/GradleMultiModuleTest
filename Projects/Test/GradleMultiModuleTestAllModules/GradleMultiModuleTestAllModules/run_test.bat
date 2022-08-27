5763d8a70632a43306c79ee6aff271eb1000db25
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2022-08-27 13:26:49 +0300

    small improvements

--- Projects/Test/GradleMultiModuleTestAllModules/GradleMultiModuleTestAllModules/run_test.bat
+++ Projects/Test/GradleMultiModuleTestAllModules/GradleMultiModuleTestAllModules/run_test.bat
@@ -1,20 +1,20 @@
 @echo off
 
 echo(
-call ./gradlew.bat clean --console=plain
+call ./gradlew clean --console=plain
 
 echo(
-call ./gradlew.bat :ModuleLow:test --console=plain ^
+call ./gradlew :ModuleLow:test --console=plain ^
 --tests com.personal.gradle.ModuleLowTest.testAppendModuleLowOther
 
 echo(
-call ./gradlew.bat :ModuleMid:test --console=plain ^
+call ./gradlew :ModuleMid:test --console=plain ^
 --tests com.personal.gradle.ModuleMidTest.testAppendModuleMid ^
 --tests com.personal.gradle.ModuleMidTest.testAppendModuleMidOther  
 
 echo(
-call ./gradlew.bat :ModuleHigh:test --console=plain ^
+call ./gradlew :ModuleHigh:test --console=plain ^
 --tests com.personal.gradle.ModuleHighTest.testAppendModuleHigh
 
 echo(
-call ./gradlew.bat :testAggregateTestReport -Dskip.tests=true --console=plain
+call ./gradlew :testAggregateTestReport -Dskip.tests=true --console=plain
